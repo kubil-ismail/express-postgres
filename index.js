2550ba9b971914e13dbac2cf118abc8d32ed7a82
Bilkis Ismail <bilkisismail07@gmail.com>
2022-06-28 14:54:18 +0700

    add multer

--- index.js
+++ index.js
@@ -4,10 +4,54 @@ const port = 8000;
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const { promisify } = require("util");
+const unlinkAsync = promisify(fs.unlink);
 
 const userRoutes = require("./routes/users/searchUsersRoutes");
 const userDetailRoutes = require("./routes/users/usersRoutes");
 const authRoutes = require("./routes/auth/index");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./images");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      `${Date.now()}_${Math.random()}_${uuidv4()}_${path.extname(
+        file.originalname
+      )}`
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1000000, // 1MB
+  },
+  fileFilter: fileFilter,
+});
+
+function fileFilter(req, file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png/;
+
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
 
 // request = input
 // response = output
@@ -33,9 +77,61 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Define all routes
+app.use("/images", express.static("images"));
 app.use("/", cors(corsOptionsDelegate), userRoutes);
 app.use("/", cors(corsOptionsDelegate), userDetailRoutes);
 app.use("/", cors(corsOptionsDelegate), authRoutes);
+
+// Upload Single Image
+app.post("/image", (req, res) => {
+  const uploadSingle = upload.single("image");
+
+  uploadSingle(req, res, (err) => {
+    try {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred when uploading.
+        res.status(400).send(err?.message ?? "Something went wrong!");
+        return;
+      } else if (err) {
+        // An unknown error occurred when uploading.
+        res.status(400).send(err ?? "Something went wrong!");
+        return;
+      }
+
+      res.send(`http://localhost:8000/${req.file?.path}`);
+    } catch (error) {
+      res.status(500).send(error?.message ?? "Upload Failed");
+    }
+  });
+});
+
+app.post("/images", (req, res) => {
+  const uploadSingle = upload.array("images", 5);
+
+  uploadSingle(req, res, (err) => {
+    try {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred when uploading.
+        res.status(400).send(err?.message ?? "Something went wrong!");
+        return;
+      } else if (err) {
+        // An unknown error occurred when uploading.
+        res.status(400).send(err ?? "Something went wrong!");
+        return;
+      }
+
+      res.send(req.files);
+      // if (err) throw err;
+    } catch (error) {
+      res.status(500).send(error?.message ?? "Upload Failed");
+    }
+  });
+});
+
+app.delete("/image", async (req, res) => {
+  await unlinkAsync(`images/${req.body.name}`);
+  res.send("DELETE COMPLETED!");
+});
 
 // end of bottom code
 app.listen(port, () => {

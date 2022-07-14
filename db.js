61b1135da683fd6342cf544a7c3bab854cea5965
Bilkis Ismail <bilkisismail07@gmail.com>
2022-07-14 13:46:29 +0700

    update

--- db.js
+++ db.js
@@ -2,21 +2,23 @@ const { Client, Pool } = require("pg");
 
 let connection;
 
-connection = new Client({
-  connectionString:
-    "postgres://dkqctycgscgiuw:beb7cf8e8ee659975959a363d716f5d6f2f0172be9993345d5f119880bace3dd@ec2-3-219-229-143.compute-1.amazonaws.com:5432/de4b9koabh5iqe",
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-// connection = new Postgre({
-//   user: "dkqctycgscgiuw",
-//   host: "ec2-3-219-229-143.compute-1.amazonaws.com",
-//   database: "de4b9koabh5iqe",
-//   password: "beb7cf8e8ee659975959a363d716f5d6f2f0172be9993345d5f119880bace3dd",
-//   port: 5432,
-// });
+if (process.env.ENV_MODE === "prod") {
+  connection = new Client({
+    connectionString:
+      "postgres://dkqctycgscgiuw:beb7cf8e8ee659975959a363d716f5d6f2f0172be9993345d5f119880bace3dd@ec2-3-219-229-143.compute-1.amazonaws.com:5432/de4b9koabh5iqe",
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  connection = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 5432,
+  });
+}
 
 connection.connect(function (err) {
   if (err) throw err;
